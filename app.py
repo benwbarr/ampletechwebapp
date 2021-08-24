fc7def0fcc138fe2c5bf22f4c9f897734e568206
unknown <benwbarr@gmail.com>
2021-08-24 10:55:02 -0700

    added base.html

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 from flask import Flask, render_template
 
-app= Flask(__name__)
+#Flask Instance
+app = Flask(__name__)
+
+#Route decorator
+#pp.route('/')
 
 @app.route('/')
+
 def index():
 	return render_template("index.html")
+
+
+@app.route('/WholesaleEWaste/')
+
+def WholesaleEWaste():
+	return render_template("WholesaleEWaste.html")
+
+@app.route('/PostAuditDismantle/')
+
+def PostAuditDismantle():
+	return render_template("PostAuditDismantle.html")
+
+@app.route('/WholesaleClientShipping/')
+
+def WholesaleClientShipping():
+	return render_template("WholesaleClientShipping.html")
+
+@app.route('/WWasteClientShipping/')
+
+def EWasteClientShipping():
+	return render_template("EWasteClientShipping.html")
